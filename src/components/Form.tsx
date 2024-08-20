a2439432aca598a039a3e8d7495ea5c452efe4e2
Franklin-Gomez <franklin_043097@hotmail.com>
2024-08-20 08:18:43 -0500

    create form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,29 @@
 
 export default function Form() {
     return (
-        <div>
+        <div className=" flex flex-col gap-3  ">
 
-            <div>
-                <label htmlFor=""> Nombre : </label>
-                <input type="text" placeholder="Nombre del Producto" />
+            <div className="  flex flex-col gap-2 ">
+                <label htmlFor="name" className="font-bold"> Nombre : </label>
+                <input 
+                    type="text" 
+                    placeholder="Nombre del Producto" 
+                    className="bg-gray-100 p-2 rounded-xl"
+                    name="name"
+                    id="name"
+                />  
             </div>
 
-            <div>
-                <label htmlFor=""> Price : </label>
-                <input type="number" placeholder="Precio del Producto" />
+            <div className=" flex flex-col gap-2">
+                <label htmlFor="price" className="font-bold"> Precio : </label>
+                <input 
+                    type="number" 
+                    placeholder="Precio del Producto" 
+                    className="bg-gray-100 p-2 rounded-xl"
+                    name="price"
+                    id="price"
+                    
+                />
             </div>
 
         </div>
--- src/views/AddProducts.tsx
+++ src/views/AddProducts.tsx
@@ -2,18 +2,35 @@ import Form from "../components/Form"
 
 export default function addProducts() {
   return (
-    <>
+
+    <div className=" bg-white rounded-2xl p-6">
+
+      <div className=" flex justify-between px-4">
+        <h1 className=" text-gray-700 font-black text-3xl" > Registrar Productos</h1>
+
+        <input 
+          type="button" 
+          value="Inicio" 
+          className="bg-slate-800 text-white rounded-xl px-4 font-black uppercase hover:bg-slate-400"
+        />
+
+      </div>
         
-        <h1> Desde formulario</h1>
-        
-        <form>
-            
-            <Form/>
+      
+      <form className="p-3">
+          
+        <Form/>
 
-            <input type="submit" />
+        <input 
+          type="submit" 
+          value={"Agregar Producto"} 
+          className=" mt-10 w-full bg-slate-800 text-white rounded-xl p-2 font-black uppercase hover:bg-slate-400 "  
+        />
 
-        </form>
+      </form>
 
-    </>
+    </div>
+
   )
+
 }
